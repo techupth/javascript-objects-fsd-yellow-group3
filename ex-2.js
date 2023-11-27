0713dfa2b9213719eec0897af719689fd44101dd
doctor <fd5@MacBook-Air-2022.local>
2023-11-27 13:36:27 +0700

    exhh

--- ex-2.js
+++ ex-2.js
@@ -5,6 +5,14 @@ const product = {
   price: 4000,
   ratings: 2.5,
   photo: null,
+  code: IT0101010,
+  socialMediaShare: 45.5,
 };
+product.ratings = 4.5;
+product.photo =
+  "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+product.price = 6000;
+delete product.ratings;
+
 let socialMediaShare = "facebookShare";
 // Start code here
